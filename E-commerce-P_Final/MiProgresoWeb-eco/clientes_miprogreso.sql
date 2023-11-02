c01e11b4d756ceb1b4dbeb5947ae9b74612142ed
RocioPulitta <109706451+RocioPulitta@users.noreply.github.com>
2023-11-02 17:52:38 -0300

    Update clientes_miprogreso.sql

--- E-commerce-P_Final/MiProgresoWeb-eco/clientes_miprogreso.sql
+++ E-commerce-P_Final/MiProgresoWeb-eco/clientes_miprogreso.sql
@@ -1,1 +1,12 @@
-ALTER USER root IDENTIFIED WITH mysql_native_password BY 'password';
+create database if not exists clientes_miprogreso;
+use clientes_miprogreso;
+create table if not exists usuarios (
+id int auto_increment primary key,
+nombre varchar(255) not null,
+apellido varchar(255)not null,
+email varchar(255)not null,
+provincia varchar(255)not null,
+localidad varchar(255)not null,
+codigo_postal int not null,
+medio_pago varchar(255) not null
+);
